de03d7db0bb9ff7228a4bfe55d76e7783f25c5a6
Trinaya10 <trinaya1010@gmail.com>
2024-02-19 22:33:42 +0530

    Added react-router-task

---
+++ React-router-task/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className="container">
+        <img src="/images/header-guvi-cover.webp" class="img-fluid" alt="Responsive image"></img>
+
+    </div>
+  )
+}
